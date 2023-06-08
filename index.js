58ac4cd81e29b9c50500a2df74f9dd9c15f64caf
lindokuhle shabalala <mlixshaba@gmail.com>
2023-06-08 21:57:18 +0200

    login

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,6 +38,7 @@ const upload = multer({ storage });
 
 app.post("/auth/register", upload.single("picture"), register);
 
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose

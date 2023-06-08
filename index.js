98462826873c76625b2a63bbcef038fbf9f5de86
lindokuhle shabalala <mlixshaba@gmail.com>
2023-06-08 20:55:42 +0200

    setup mongoose

--- index.js
+++ index.js
@@ -33,4 +33,17 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-app.listen(() => console.log(`Server is running on port 3000`));
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
